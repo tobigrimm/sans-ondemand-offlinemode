7606581d6da425db47dbe80d12d865627a568ed1
Tobi Grimm <tobi-git@grimm.gr>
2019-09-27 14:39:14 +0200

    parse all the things

--- sans-on-demand-video-backup.py
+++ sans-on-demand-video-backup.py
@@ -18,11 +18,40 @@ check_for_python3()
 
 def parse_json(jsondata):
     coursedata = json.load(jsondata)
-    print("name: %s" % coursedata['course']['name'])
+    print("Now downloading: %s" % coursedata['course']['name'])
     name = coursedata['course']['name']
     sections = coursedata['course']['childNodes']
 
-def download_if_na(link, cookie, useragent):
+    # get chapters
+    for section in sections:
+        sectionname = section['name']
+        print(sectionname)
+        subsections = section['childNodes']
+        
+        for subsection in subsections:
+
+            # should be the same as sectionname
+            subsectionname = subsection['name']
+            try:
+                assert subsectionname == sectionname
+            except:
+                print("something wen't wrong while parsing, please send the parsed json File to the author")
+            
+            chapters = subsection['learningObjects']
+
+            for chapter in chapters:
+                chapterdata = chapter['metadata']
+                chaptername = chapterdata['name']
+                chapterduration = chapterdata['durationSeconds']
+                chapterbaseurl = chapterdata['baseUrl']
+                chaptercookies = chapterdata['cookies']
+                print("\tChapter %s:" % chaptername)
+
+                slideurl = chapterbaseurl + "/script.json"
+
+
+def dump_em_all(link, cookie, useragent):
+    # TODO check before downloading if file already exists
     requests.get("http://example.com", headers={ "user-agent": "The Coolest Useragent" })
 
 if __name__ == "__main__":
@@ -30,6 +59,7 @@ if __name__ == "__main__":
     argparser.add_argument("-q", "--quality", choices = ["SD","HD"], default="HD", help="Quality to download the material with from SANS. Available choices are SD and HD")
     argparser.add_argument("-d","--debug",default=False)
     argparser.add_argument("--useragent", default="Mozilla/5.0 (X11; Linux x86_64; rv:69.0) Gecko/20100101 Firefox/69.0")
+    argparser.add_argument("-o","--output", default="dumps", metavar="outputdir", help="Directory for the output of the SANS course, default is dumps. Folder will be created if it does not already exist.")
     argparser.add_argument("jsonfile", metavar="jsonfile", help="Dumped jsonfile from the uberRequest on SANS On Demand Player")
     args = argparser.parse_args()
     # paremeters/options
@@ -37,6 +67,7 @@ if __name__ == "__main__":
     json_inputfile = args.jsonfile# json file containing the course info and the cookies needed to download them
     debug_mode = args.debug
     useragent = args.useragent
+    outputdir = args.output
 
     try:
         with open(json_inputfile, "r") as json_input:
